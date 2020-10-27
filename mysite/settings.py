d50115a67d9c7f5252baf67c0c0fb76862daff1f
RJ0887 <57557580+RJ0887@users.noreply.github.com>
2020-10-27 22:36:37 +0800

    update

--- mysite/settings.py
+++ mysite/settings.py
@@ -80,8 +80,14 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'todoDB',
+        'USER':'admin',
+        'PASSWORD':'admin',
+        'HOST':'172.25.8.19',
+        'PORT':'5432',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
